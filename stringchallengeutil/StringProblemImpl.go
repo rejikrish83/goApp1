666508d01adb478a6a6d7fd9cf18ca5c69a1f5c0
rejikrish83 <rejikrish83@gmail.com>
2024-01-08 15:32:10 +0000

    Convert the out put to lowercase

--- stringchallengeutil/StringProblemImpl.go
+++ stringchallengeutil/StringProblemImpl.go
@@ -1,24 +1,27 @@
 package stringchallengeutil
 
-import("fmt")
+import (
+	"fmt"
+	"strings"
+)
 
-type StringReverse struct{
+type StringReverse struct {
 	Actual string
 }
 
-func (stringRev StringReverse) ReverseString() string{
-	runes := []rune (stringRev.Actual)
+func (stringRev StringReverse) ReverseString() string {
+	runes := []rune(stringRev.Actual)
 
 	length := len(runes)
 
-	for i , j := 0, length-1; i<j;i,j = i+1, j-1{
+	for i, j := 0, length-1; i < j; i, j = i+1, j-1 {
 		runes[i], runes[j] = runes[j], runes[i]
 	}
 
-	return string(runes)
+	return strings.ToLower(string(runes))
 }
 
-func PrintStringValue(stringValue StringProblemInterface){
+func PrintStringValue(stringValue StringProblemInterface) {
 
 	fmt.Println(stringValue.ReverseString())
 
